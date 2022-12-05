ad50bdb6a058a484c1066f212437a99d4cf69f98
unknown <rabiul0420@gmail.com>
2022-12-05 09:44:49 +0600

    plugin activation

--- includes/class-plugin-activator.php
+++ includes/class-plugin-activator.php
@@ -22,13 +22,25 @@ class WBTM_Plugin_Activator{
           $bus_search_page = array(
           'post_type' => 'page',
           'post_name' => 'bus-search-list',
-          'post_title' => 'Bus Search',
+          'post_title' => 'Bus Search result',
           'post_content' => '[wbtm-bus-search]',
           'post_status' => 'publish',
           );
 
           wp_insert_post($bus_search_page);
       }
+
+        if (! (new self())->wbtm_get_page_by_slug('bus-global-search')) {
+            $bus_global_search_page = array(
+                'post_type' => 'page',
+                'post_name' => 'bus-global-search',
+                'post_title' => 'Global search form',
+                'post_content' => '[wbtm-bus-search-form]',
+                'post_status' => 'publish',
+            );
+
+            wp_insert_post($bus_global_search_page);
+        }
 
 
       
