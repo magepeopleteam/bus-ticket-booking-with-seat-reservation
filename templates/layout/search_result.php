e5b46049d6b2ee1308b1beabbc088d0fc91a345e
raselsha <raselsha@gmail.com>
2023-11-28 10:22:47 +0600

    desktop view and css done with old table

--- templates/layout/search_result.php
+++ templates/layout/search_result.php
@@ -15,10 +15,84 @@
 	if (sizeof($bus_ids) > 0) {
 		$bus_count = 0;
 		?>
+
+		<!-- new layout -->
 		<div class="wbtm_bus_list_area">
 			<input type="hidden" name="wbtm_start_route" value="<?php echo esc_attr($start_route); ?>"/>
 			<input type="hidden" name="wbtm_end_route" value="<?php echo esc_attr($end_route); ?>"/>
 			<input type="hidden" name="wbtm_date" value="<?php echo esc_attr(date('Y-m-d', strtotime($date))); ?>"/>
+			
+			<?php foreach ($bus_ids as $bus_id) : ?>
+				<?php  
+					$all_info = WBTM_Functions::get_bus_all_info($bus_id, $date, $start_route, $end_route); 
+					if (sizeof($all_info) > 0) :
+						$bus_count++;
+						$price = $all_info['price'];
+				?>
+					<div class="wbtm-bust-list">
+						<div class="wbtm-bus-image">
+							<img src="https://dummyimage.com/100" alt="">
+						</div>
+						<div class="wbtm-bus-name text-start">
+							<h5 class="_textTheme" data-href="<?php echo esc_attr(get_the_permalink($bus_id)); ?>"><?php echo get_the_title($bus_id); ?></h5>
+							<p><?php echo esc_html(MP_Global_Function::get_post_info($bus_id, 'wbtm_bus_no')); ?></p>
+						</div>
+						<div class="wbtm-bus-route text-start">
+							<h6>
+								<span class="fa fa-map-pin"></span>
+								<?php echo esc_html($all_info['bp']) . ' ' . esc_html($all_info['bp_time'] ? ' (' . MP_Global_Function::date_format($all_info['bp_time'], 'time') . ' )' : ''); ?>
+							</h6>
+							<h6>
+								<i class="fas fa-map-marker-alt"></i>
+								<?php echo esc_html($all_info['dp']) . ' ' . esc_html($all_info['dp_time'] ? ' (' . MP_Global_Function::date_format($all_info['dp_time'], 'time') . ' )' : ''); ?>
+							</h6>
+						</div>
+						<div class="wbtm-seat-info text-center">
+							<div>
+								<h6><?php echo MP_Global_Function::get_post_info($bus_id, 'wbtm_bus_category'); ?></h6>
+								<p><?php echo WBTM_Translations::text_coach_type(); ?></p>
+							</div>
+							<div>
+								<h6><?php echo esc_html($all_info['available_seat']); ?>/<?php echo esc_html($all_info['total_seat']); ?></h6>
+								<p><?php echo WBTM_Translations::text_available(); ?></p>
+							</div>
+							<div>
+								<h6><?php echo wc_price($price); ?></h6>
+								<p><?php echo WBTM_Translations::text_fare().'/' . WBTM_Translations::text_seat(); ?></p>
+							</div>
+						</div>
+						<div class="wbtm-seat-book">
+							<button type="button" class="_dButton_xs" id="get_wbtm_bus_details"
+								data-bus_id="<?php echo esc_attr($bus_id); ?>"
+								data-open-text="<?php echo esc_attr(WBTM_Translations::text_view_seat()); ?>"
+								data-close-text="<?php echo esc_attr(WBTM_Translations::text_close_seat()); ?>"
+								data-add-class="mActive">
+								<span data-text><?php echo esc_html(WBTM_Translations::text_view_seat()); ?></span>
+							</button>
+						</div>
+					</div>
+					<table>
+						<tbody>
+							<tr data-row_id="<?php echo esc_attr($bus_id); ?>">
+								<td colspan="9" class="wbtm_bus_details"></td>
+							</tr>
+						</tbody>
+					</table>
+				<?php endif; ?>
+			<?php endforeach; ?>
+			<?php if ($bus_count == 0) : ?>
+				<div>
+					<?php WBTM_Layout::msg(WBTM_Translations::text_no_bus()); ?>
+				</div>
+			<?php endif; ?>
+		</div>
+
+			<!-- new layout -->
+		<div class="wbtm_bus_list_area">
+			<input type="hidden" name="wbtm_start_route" value="<?php echo esc_attr($start_route); ?>"/>
+			<input type="hidden" name="wbtm_end_route" value="<?php echo esc_attr($end_route); ?>"/>
+			<input type="hidden" name="wbtm_date" value="<?php echo esc_attr(date('Y-m-d', strtotime($date))); ?>"/>
+
 			<table class="_layoutFixed">
 				<thead>
 				<tr>
