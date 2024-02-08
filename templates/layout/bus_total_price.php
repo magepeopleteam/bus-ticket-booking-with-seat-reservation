8080a5e3848cffa503e74d157a44d6d8379f2bd8
raselsha <raselsha@gmail.com>
2024-02-08 14:17:59 +0600

    refactor: sub total padding removed

--- templates/layout/bus_total_price.php
+++ templates/layout/bus_total_price.php
@@ -7,7 +7,7 @@
 		die;
 	} // Cannot access pages directly.
 ?>
-	<div class="justifyStart padding_xs">
+	<div class="justifyStart pT_xs">
 		<h5 class="col_6"><?php echo WBTM_Translations::text_ticket_sub_total(); ?></h5>
 		<h5 class="wbtm_sub_total col_6 paddingLeft_xs"><?php echo wc_price(0); ?></h5>
 	</div>
