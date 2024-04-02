0ac9e9ac6846cbbb653541bafce24ee2df7f73a3
raselsha <raselsha@gmail.com>
2024-04-02 09:27:05 +0600

    refactor: customer email input box update

--- templates/layout/bus_total_price.php
+++ templates/layout/bus_total_price.php
@@ -7,7 +7,10 @@
 		die;
 	} // Cannot access pages directly.
 ?>
-	<div class="justifyStart pT_xs">
-		<h5 class="col_6"><?php echo WBTM_Translations::text_ticket_sub_total(); ?></h5>
-		<h5 class="wbtm_sub_total col_6 paddingLeft_xs"><?php echo wc_price(0); ?></h5>
-	</div>
+
+	<table class="_layoutFixed_dLayout_mB_xs">
+		<tr>
+			<td width="50%"><h5 class="col_6"><?php echo WBTM_Translations::text_ticket_sub_total(); ?></h5></td>
+			<td><h5 class="wbtm_sub_total col_6 paddingLeft_xs"><?php echo wc_price(0); ?></h5></td>
+		</tr>
+	</table>
--- templates/layout/extra_service.php
+++ templates/layout/extra_service.php
@@ -50,6 +50,7 @@
 					</table>
 				</div>
 			</div>
+			<!-- customer email input box will show here if admin back order -->
 			 <?php do_action('wbtm_registration_form_inside', $post_id); ?>
 			<?php
 		}
