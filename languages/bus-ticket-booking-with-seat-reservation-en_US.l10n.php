f66e185cbbf77705551f759c0dde3d2a6edba7ca
Hamid Hosen Azad <hamidxazad@gmail.com>
2024-11-25 12:14:21 +0600

    languages folder updated

---
+++ languages/bus-ticket-booking-with-seat-reservation-en_US.l10n.php
@@ -0,0 +1,2 @@
+<?php
+return ['project-id-version'=>'Bus Ticket Booking with Seat Reservation','report-msgid-bugs-to'=>'','pot-creation-date'=>'2023-01-27 06:21+0000','po-revision-date'=>'2024-11-25 06:08+0000','last-translator'=>'','language-team'=>'English (United States)','language'=>'en-US','plural-forms'=>'nplurals=2; plural=n != 1;','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','x-generator'=>'Loco https://localise.biz/','x-loco-version'=>'2.6.14; wp-6.7.1','messages'=>[]];
