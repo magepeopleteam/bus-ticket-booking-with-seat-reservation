5eb93849880a5aa6cc2a09e5c5d2468316d085bb
Shahadat <raselsha@gmail.com>
2025-05-14 13:00:43 +0600

    style: details seat

--- templates/layout/bus_total_price.php
+++ templates/layout/bus_total_price.php
@@ -10,7 +10,7 @@
 
 	<table class="_layoutFixed_dLayout_mB_xs">
 		<tr>
-			<td width="50%"><h5 class="col_6"><?php echo WBTM_Translations::text_ticket_sub_total(); ?></h5></td>
-			<td><h5 class="wbtm_sub_total col_6 paddingLeft_xs"><?php echo wc_price(0); ?></h5></td>
+			<td colspan="2"><h5 class="col_6"><?php echo WBTM_Translations::text_ticket_sub_total(); ?></h5></td>
+			<td colspan="2" style="text-align: left;"><h5 class="wbtm_sub_total"><?php echo wc_price(0); ?></h5></td>
 		</tr>
 	</table>
