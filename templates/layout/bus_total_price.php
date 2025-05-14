1449653f51d21837e2c5a7af0c19f580973fa107
Shahadat <raselsha@gmail.com>
2025-05-14 14:36:11 +0600

    style: responsive issue need to fix

--- templates/layout/bus_total_price.php
+++ templates/layout/bus_total_price.php
@@ -8,7 +8,7 @@
 	} // Cannot access pages directly.
 ?>
 
-	<table class="_layoutFixed_dLayout_mB_xs">
+	<table class="_layoutFixed_dLayout_mB_xs wbtm_totals">
 		<tr>
 			<td colspan="2"><h5 class="col_6"><?php echo WBTM_Translations::text_ticket_sub_total(); ?></h5></td>
 			<td colspan="2" style="text-align: left;"><h5 class="wbtm_sub_total"><?php echo wc_price(0); ?></h5></td>
